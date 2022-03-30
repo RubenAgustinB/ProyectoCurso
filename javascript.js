d3afdb4acd392bddfc4b9990c56590dd00cad567
unknown <zeodcrew@gmail.com>
2022-03-30 14:44:53 -0300

    inicio de trabajo final

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let botonplay=document.querySelector('.botonPlay')
 let botonpause=document.querySelector('.botonPause')
 
 let video=document.querySelector('video')
-
+let texto="Texto h2"
 botonplay.addEventListener('click',()=>{
     video.play()
 })
